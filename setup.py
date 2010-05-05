a7ff9d751bdfeffdec2c6ea98f9a17afd4793e18
holger krekel <holger@merlinux.eu>
2010-05-05 20:19:30 +0200

    require execnet-1.0.6 - it's release already

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     packages = ['xdist'],
     entry_points = {'pytest11': ['xdist = xdist.plugin'],},
     zip_safe=False,
-    install_requires = ['execnet>=1.0.5', 'py>=1.3.0'],
+    install_requires = ['execnet>=1.0.6', 'py>=1.3.0'],
     classifiers=[
     'Development Status :: 4 - Beta',
     'Intended Audience :: Developers',
