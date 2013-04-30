df66687eaee2930aa4c08d106a84a52645a3738b
holger krekel <holger@merlinux.eu>
2013-04-30 17:08:06 +0200

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="pytest-xdist",
-    version='1.9dev1',
+    version='1.9dev2',
     description='py.test xdist plugin for distributed testing and loop-on-failing modes',
     long_description=open('README.txt').read(),
     license='MIT',
