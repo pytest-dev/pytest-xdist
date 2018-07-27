ee0754bc35628b754da3ca02f7520ef04d59c3e0
Bruno Oliveira <bruno@esss.com.br>
2018-07-27 08:24:34 -0300

    Improve test for Travis fallback

--- testing/test_plugin.py
+++ testing/test_plugin.py
@@ -45,10 +45,10 @@ def test_auto_detect_cpus(testdir, monkeypatch):
     config = testdir.parseconfigure("-nauto")
     assert config.getoption('numprocesses') == 99
 
-    if not hasattr(os, 'sched_getaffinity'):
-        monkeypatch.setenv('TRAVIS', 'true')
-        config = testdir.parseconfigure("-nauto")
-        assert config.getoption('numprocesses') == 2
+    monkeypatch.delattr(os, 'sched_getaffinity', raising=False)
+    monkeypatch.setenv('TRAVIS', 'true')
+    config = testdir.parseconfigure("-nauto")
+    assert config.getoption('numprocesses') == 2
 
 
 def test_boxed_with_collect_only(testdir):
