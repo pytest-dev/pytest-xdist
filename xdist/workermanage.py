22e36c9cb401c0c0a7b77d232c9f093525697f0a
Brandon Hoffman <brandon.michael.hoffman@gmail.com>
2018-09-27 19:32:10 -0500

    run black formatter

--- xdist/workermanage.py
+++ xdist/workermanage.py
@@ -429,9 +429,11 @@ def unserialize_warning_message(data):
         try:
             message = cls(*data["message_args"])
         except TypeError:
-            message_text = "{mod}.{class}: {msg}".format(mod=data["message_module"],
-                                                         cls=data["message_class_name"],
-                                                         msg=data["message_str"])
+            message_text = "{mod}.{class}: {msg}".format(
+                mod=data["message_module"],
+                cls=data["message_class_name"],
+                msg=data["message_str"],
+            )
             message = Warning(message_text)
     else:
         message = data["message_str"]
