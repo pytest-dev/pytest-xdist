4406042ce9a0d0bb45956b95f7adbe6875eae56f
Bruno Oliveira <nicoddemus@gmail.com>
2020-08-05 20:26:10 -0300

    Add tests

--- 504.feature
+++
@@ -1,1 +0,0 @@
-New methods `is_xdist_worker`, `is_xdist_master`, `get_xdist_worker_id`, so as to easily grab info about current node.
--- src/xdist/plugin.py
+++ src/xdist/plugin.py
@@ -199,23 +199,21 @@ def pytest_cmdline_main(config):
 # -------------------------------------------------------------------------
 
 
-def is_xdist_worker(request_or_session):
+def is_xdist_worker(request_or_session) -> bool:
     """Return `True` if this is an xdist worker, `False` otherwise
 
     :param request_or_session: the `pytest` `request` or `session` object
-    :return:
     """
     return hasattr(request_or_session.config, "workerinput")
 
 
-def is_xdist_master(request_or_session):
+def is_xdist_master(request_or_session) -> bool:
     """Return `True` if this is the xdist master, `False` otherwise
 
     Note: this method also returns `False` when distribution has not been
     activated at all.
 
     :param request_or_session: the `pytest` `request` or `session` object
-    :return:
     """
     return (
         not is_xdist_worker(request_or_session)
@@ -223,18 +221,18 @@ def is_xdist_master(request_or_session):
     )
 
 
-def get_xdist_worker_id(request_or_session):
+def get_xdist_worker_id(request_or_session) -> str:
     """Return the id of the current worker ('gw0', 'gw1', etc) or 'master'
-    if running on the master node.
+    if running on the 'master' node.
+
+    If not distributing tests (for example passing `-n0` or not passing `-n` at all)
+    also return 'master'.
 
     :param request_or_session: the `pytest` `request` or `session` object
-    :return:
     """
     if hasattr(request_or_session.config, "workerinput"):
         return request_or_session.config.workerinput["workerid"]
     else:
-        # TODO shall we raise an exception if dist is not enabled ?
-        #  i.e. `not is_xdist_master(request_or_session)` ?
         return "master"
 
 
