6d90f72cb2132852fb348b46f107438f726e9f19
Kurt Godwin <kgodwin@itron.com>
2023-03-03 13:07:17 -0500

    added logger

--- src/xdist/dsession.py
+++ src/xdist/dsession.py
@@ -10,7 +10,8 @@ from xdist.scheduler import (
     LoadGroupScheduling,
     WorkStealingScheduling,
 )
-
+import logging
+import pickle
 
 from queue import Empty, Queue
 
@@ -275,6 +276,17 @@ class DSession:
         rep.node = node
         self.config.hook.pytest_runtest_logreport(report=rep)
         self._handlefailures(rep)
+
+    def worker_runtest_logmessage(self, node, record):
+        record = pickle.loads(record)
+        for handler in logging.getLogger().handlers:
+
+            if not True: #self.respect_handler_level:
+                process = True
+            else:
+                process = record.levelno >= handler.level
+            if process:
+                handler.handle(record)
 
     def worker_runtest_protocol_complete(self, node, item_index, duration):
         """
