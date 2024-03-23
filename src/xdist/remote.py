594ba7ad2311663167e1099d033cd0d7dfaa7bf5
Ben Brown <ben@beninter.net>
2024-03-23 16:28:27 -0400

    Revert "New solution that does not require change to dsession"
    This reverts commit c7a19c3d290e0a1eca0703c7d5d51fc7bef7cbbe.

--- src/xdist/remote.py
+++ src/xdist/remote.py
@@ -154,10 +154,12 @@ class WorkerInteractor:
         self.channel.setcallback(self.handle_command, endmarker=self.SHUTDOWN_MARK)
         self.nextitem_index = self._get_next_item_index()
         while self.nextitem_index is not self.SHUTDOWN_MARK:
-            self.run_one_test(session.shouldfail or session.shouldstop)
+            self.run_one_test()
+            if session.shouldfail or session.shouldstop:
+                break
         return True
 
-    def run_one_test(self, skip: bool):
+    def run_one_test(self):
         self.item_index = self.nextitem_index
         self.nextitem_index = self._get_next_item_index()
 
@@ -171,10 +173,7 @@ class WorkerInteractor:
         worker_title("[pytest-xdist running] %s" % item.nodeid)
 
         start = time.time()
-
-        if not skip:
-            self.config.hook.pytest_runtest_protocol(item=item, nextitem=nextitem)
-
+        self.config.hook.pytest_runtest_protocol(item=item, nextitem=nextitem)
         duration = time.time() - start
 
         worker_title("[pytest-xdist idle]")
