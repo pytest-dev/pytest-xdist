cd857184d6adaf6afd7725df21e1dbefd8191ecc
Cornelius Riemenschneider <cornelius@github.com>
2024-08-21 16:02:53 +0200

    Make node ID modification more stable.
    We force test collection on the controller node, as we run some global
    setup there before starting the workers. That setup also depends on the
    test node IDs, but currently, pytest-xdist only modifies the test node IDs
    on the workers, that've been started and where `setup_config` has been called.
    I don't see a good reason for this, and for creating a separate config entry,
    so let's just check the right condition in the pytest hook.
    That way, the hook will always do the same node ID transformation, regardless
    of pytest-xdist setup.

--- src/xdist/remote.py
+++ src/xdist/remote.py
@@ -207,7 +207,7 @@ class WorkerInteractor:
         items: list[pytest.Item],
     ) -> None:
         # add the group name to nodeid as suffix if --dist=loadgroup
-        if config.getvalue("loadgroup"):
+        if config.getoption("dist") == "loadgroup":
             for item in items:
                 mark = item.get_closest_marker("xdist_group")
                 if not mark:
@@ -356,7 +356,6 @@ def getinfodict() -> WorkerInfo:
 
 
 def setup_config(config: pytest.Config, basetemp: str | None) -> None:
-    config.option.loadgroup = config.getvalue("dist") == "loadgroup"
     config.option.looponfail = False
     config.option.usepdb = False
     config.option.dist = "no"
